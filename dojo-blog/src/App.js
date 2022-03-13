03141cf5ee0fba42661b9c981573ab2220264375
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-13 09:03:38 -0600

    Create navbar and home components

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,28 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from './Home'
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const google = "https://www.google.com";
-  // const person = { name: 'lola', age: 30};
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked { likes } times</p>
-
-        <p>{10}</p>
-        <p>"Hello friends"</p>
-        <p>[1, 2, 3, 4, 5]</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={google} target="_blank">Go to Google</a>
-
+        <Home />
       </div>
-        
     </div>
   );
 }
