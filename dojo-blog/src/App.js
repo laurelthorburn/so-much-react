50fdb449a1a592fa6d595106632f881ad01a84da
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-13 08:56:08 -0600

    Playing in App.js

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,22 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const google = "https://www.google.com";
+  // const person = { name: 'lola', age: 30};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked { likes } times</p>
+
+        <p>{10}</p>
+        <p>"Hello friends"</p>
+        <p>[1, 2, 3, 4, 5]</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={google} target="_blank">Go to Google</a>
+
+      </div>
+        
     </div>
   );
 }
